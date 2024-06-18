b007fc0dcbda0afa2579731bbb5eecd1fb311b41
olhkyle <kylekwon96@gmail.com>
2024-06-18 16:22:36 +0900

    [CHORE] add property in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,18 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'prettier',
+		'plugin:import/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+	],
+
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'import', 'prettier', 'react', 'react-hooks', '@emotion', 'react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+	},
+};
